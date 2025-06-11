4208d5bee329f216032d9a0d45807329fbaa7041
Andreasafent <afentoulidisandreas@gmail.com>
2025-06-11 20:26:46 +0300

    feat: refactor CountryListing component to accept country object; implement data fetching in Home component using axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,34 @@
 import SearchBar from "../components/SearchBar";
 import Filter from "../components/FIlter";
-import data from "../data.json";
 import CountryListing from "../components/CountryListing";
+import axios from 'axios'; // Uncomment if you need to use axios directly in this file
+import { useEffect, useState } from "react";
 
 function Home() {
-    console.log(data[0].flag)
+
+    let url = 'https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags';
+    const [countryData, setCountryData] = useState([]); // Assuming you have a state management setup like useState or useContext
+
+
+
+    useEffect(() => {
+        axios.get(url)
+            .then(response => {
+                setCountryData(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the data!", error);
+            })
+            .finally(() => {
+                console.log("Fetch attempt completed.");
+            });
+    }, []);
+
+
+
     return (
         <>
-            <main className="flex flex-col sm:min-w-[80%] h-full text-black px-4 py-8 dark:text-white ">
+            <main className="flex flex-col max-w-[1280px] sm:min-w-[80%]  h-full text-black px-4 py-8 dark:text-white ">
                 <div className="flex flex-col gap-5 sm:flex-row sm:justify-between sm:items-between  items-start ">
 
                     <SearchBar />
@@ -17,15 +38,10 @@ function Home() {
                 </div>
 
                 <div className="flex gap-10 md:justify-between justify-center flex-wrap pt-10">
-                    {
-                        data.map((_, index, array) => (
-                            <CountryListing
-                                key={index}
-                                index={index}
-                                array={array}
-                            />
-                        ))
-                    }
+                    {countryData.map((country, index) => (
+                        <CountryListing key={index} country={country} />
+                    ))}
+                    
                 </div>
 
 
